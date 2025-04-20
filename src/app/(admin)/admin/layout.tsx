c47a6a098d4a9f791621e0d6d92ae843791093f2
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2025-04-20 18:00:10 +0500

    fix: fix layout

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -1,5 +1,6 @@
 import type React from "react";
 import type { Metadata } from "next";
+import "./globals.css";
 import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
 import { AdminSidebar } from "@/features/admin/sidebar/admin-sidebar";
 
@@ -8,19 +9,24 @@ export const metadata: Metadata = {
   description: "Admin dashboard for managing your application",
 };
 
+// src/app/(admin)/layout.tsx
 export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <SidebarProvider defaultOpen={true}>
-      <div className="flex h-screen">
-        <AdminSidebar />
-        <SidebarInset className="flex-1 overflow-auto">
-          <main className="h-full bg-muted/10">{children}</main>
-        </SidebarInset>
-      </div>
-    </SidebarProvider>
+    <html lang="ru">
+      <body>
+        <SidebarProvider defaultOpen={true}>
+          <div className="flex h-screen">
+            <AdminSidebar />
+            <SidebarInset className="flex-1 overflow-auto">
+              <main className="h-full bg-muted/10">{children}</main>
+            </SidebarInset>
+          </div>
+        </SidebarProvider>
+      </body>
+    </html>
   );
 }
