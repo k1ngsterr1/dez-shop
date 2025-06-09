c0e670576cf4182996690bdd1be4356098cac1f0
Erlan-Bam <erlanzh.gg@gmail.com>
2025-06-09 23:30:09 +0500

    fix: disable additional TypeScript and React linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,9 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
     },
   },
 ];
